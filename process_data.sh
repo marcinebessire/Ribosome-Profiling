227434509cdd5822eefee30aa6447f8e217cb92a
Marcine Bessire <m.bessire@hotmail.com>
2023-11-16 11:00:42 +0100

    here is your first file

---
+++ process_data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd /data/users/mbessire/RNA_seq/exercise_data/un
+
+# Clip 3' adapter
+for x in $(ls -d *.fastq.gz); do
+    echo ${x}
+    cutadapt \
+        -j 4 \
+        -a CTGTAGGCACCATCAAT \
+        -q 25 \
+        --minimum-length 25 \
+        --discard-untrimmed \
+        -o $(basename ${x} .fastq.gz)_clpd.fastq.gz \
+        ${x} 1> $(basename ${x} .fastq.gz)_clpd_log.txt
+done
+
+# Trim 4 nt (randomized bases) from the 3' end
+for x in $(ls -d *_clpd.fastq.gz); do
+    echo ${x}
+    cutadapt \
+        -j 4 \
+        -q 25 \
+        --cut -4 \
+        --minimum-length 25 \
+        -o $(basename ${x} .fastq.gz)_tr.fastq.gz \
+        ${x} 1> $(basename ${x} .fastq.gz)_tr_log.txt
+done
+
+
+
