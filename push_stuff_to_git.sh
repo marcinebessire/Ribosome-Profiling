12c6a0f9a5da321b1b38a4ea5750860271ad3a24
Marcine Bessire <m.bessire@hotmail.com>
2023-11-16 11:10:45 +0100

    jetzt steht da etwas anderess

--- push_stuff_to_git.sh
+++ push_stuff_to_git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git add . # add all files in the current directory to the git commit
-git commit -m "this is a commit" # create the commit and add the commit message "this is a commit"
+git commit -m "jetzt steht da etwas anderess" # create the commit and add the commit message "this is a commit"
 git push # push all commits to git that are not on git yet
 # note doing it this way is very bad practice as the commit message has no meaning and pushing evry 
 #file that was changes makes a mess when working with multiple people
