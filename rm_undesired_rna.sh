4c55b517f16a784718eb19d1bdbbbca59fbad9c6
Marcine Bessire <m.bessire@hotmail.com>
2023-12-01 14:54:34 +0100

    revised

--- rm_undesired_rna.sh
+++ rm_undesired_rna.sh
@@ -1,15 +1,14 @@
 cd /data/users/mbessire/RNA_seq/exercise_data/unprocessedWTKO
 
-# Mapping to undesired RNAs
 for x in $(ls -d *tr.fastq.gz); do
-    echo "Processing: ${x}" #added line for debugging
-    gunzip -cd "${x}" | head  # Print the first few lines of the decompressed file
+    echo ${x}; \
+    gunzip -cd ${x} | \
     bowtie \
-    -S \
-    -t \
-    -p 4 \
-    /data/users/mbessire/RNA_seq/exercise_data/bowtie_files/GRCh38_p13_r_t_sno_sn_RNA_ENSEMBL_NCBI_GtRNAdb \
-    - \
-    --un "$(basename ${x} .fastq.gz)_no_r_t_sno_sn_RNA.fastq" 2> "$(basename ${x} .fastq.gz)_no_r_t_sno_sn_RNA_log.txt" > /dev/null
+        -S \
+        -t \
+        -p 4 \
+        /data/users/mbessire/RNA_seq/exercise_data/bowtie_files_undesired/GRCh38_p13_r_t_sno_sn_RNA_ENSEMBL_NCBI_GtRNAdb \
+        - \
+        --un $(basename ${x} .fastq.gz)_no_r_t_sno_sn_RNA.fastq 2> $(basename ${x} .fastq.gz)_no_r_t_sno_sn_RNA_log.txt > /dev/null
 done
 
