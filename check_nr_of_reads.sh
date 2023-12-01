4c4efc4f37b345b1bd50f12d781ead766fe183fb
Marcine Bessire <m.bessire@hotmail.com>
2023-12-01 15:39:49 +0100

    revised

---
+++ check_nr_of_reads.sh
@@ -0,0 +1,11 @@
+# Check the number of reads mapped to the genome
+module load UHTS/Analysis/samtools/1.8
+cd /data/users/mbessire/RNA_seq/exercise_data/removed_undesired_RNA
+
+for x in $(ls -d *_sorted.bam); do
+    echo "Checking alignment statistics for ${x}"
+    
+    # Display alignment statistics using samtools flagstat
+    samtools flagstat ${x}
+done
+
