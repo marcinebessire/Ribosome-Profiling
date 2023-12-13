029a280aea0c30da4cbd0c46d2558bc68bdb0b4f
Marcine Bessire <m.bessire@hotmail.com>
2023-12-13 20:46:24 +0100

    revised

--- Codon_occupancy_generation.sh
+++ Codon_occupancy_generation.sh
@@ -1,24 +1,26 @@
+chmod +x Codon_occupancy_generation.sh
+./Codon_occupancy_generation.sh
 
 ./Codon_occupancy_cal.sh \
-/PATH/reference/GRCh38_APPRIS_CDS_18_SingleLine.fa \
-/PATH/RPF_KO_Rep1_clpd_tr_no_r_t_sno_sn_RNA_GRCh38_APPRIS_CDS.sam
+/data/users/mbessire/RNA_seq/exercise_data/indices/GRCh38_APPRIS_CDS_18_SingleLine.fa \
+/data/users/mbessire/RNA_seq/exercise_data/removed_undesired_RNA/RPF_KO_Rep1_clpd_tr_no_r_t_sno_sn_RNA_GRCh38_APPRIS_CDS.sam
 
 mv ./Codon_occupancy.txt ./RPF_KO_Rep1_Codon_occupancy.txt
 
 ./Codon_occupancy_cal.sh \
-/PATH/reference/GRCh38_APPRIS_CDS_18_SingleLine.fa \
-/PATH/RPF_KO_Rep2_clpd_tr_no_r_t_sno_sn_RNA_GRCh38_APPRIS_CDS.sam
+/data/users/mbessire/RNA_seq/exercise_data/indices/GRCh38_APPRIS_CDS_18_SingleLine.fa \
+/data/users/mbessire/RNA_seq/exercise_data/removed_undesired_RNA/RPF_KO_Rep2_clpd_tr_no_r_t_sno_sn_RNA_GRCh38_APPRIS_CDS.sam
 
 mv ./Codon_occupancy.txt ./RPF_KO_Rep2_Codon_occupancy.txt
 
 ./Codon_occupancy_cal.sh \
-/PATH/reference/GRCh38_APPRIS_CDS_18_SingleLine.fa \
-/PATH/RPF_WT_Rep1_clpd_tr_no_r_t_sno_sn_RNA_GRCh38_APPRIS_CDS.sam
+/data/users/mbessire/RNA_seq/exercise_data/indices/GRCh38_APPRIS_CDS_18_SingleLine.fa \
+/data/users/mbessire/RNA_seq/exercise_data/removed_undesired_RNA/RPF_WT_Rep1_clpd_tr_no_r_t_sno_sn_RNA_GRCh38_APPRIS_CDS.sam
 
 mv ./Codon_occupancy.txt ./RPF_WT_Rep1_Codon_occupancy.txt
 
 ./Codon_occupancy_cal.sh \
-/PATH/reference/GRCh38_APPRIS_CDS_18_SingleLine.fa \
-/PATH/RPF_WT_Rep2_clpd_tr_no_r_t_sno_sn_RNA_GRCh38_APPRIS_CDS.sam
+/data/users/mbessire/RNA_seq/exercise_data/indices/GRCh38_APPRIS_CDS_18_SingleLine.fa \
+/data/users/mbessire/RNA_seq/exercise_data/removed_undesired_RNA/RPF_WT_Rep2_clpd_tr_no_r_t_sno_sn_RNA_GRCh38_APPRIS_CDS.sam
 
 mv ./Codon_occupancy.txt ./RPF_WT_Rep2_Codon_occupancy.txt
