b89dad0416df78bcca0fa5d471778bd180430315
Marcine <m.bessire@hotmail.com>
2024-01-26 10:08:51 +0100

    new comment

---
+++ riboseQC.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Convert genome FASTA file to 2bit format
+
+# Download fatoTwoBit:
+
+# For linux (http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/faToTwoBit)
+# For Mac (http://hgdownload.soe.ucsc.edu/admin/exe/macOSX.x86_64/faToTwoBit)
+# Other platforms (http://hgdownload.soe.ucsc.edu/admin/exe/)
+
+# Make sure you have faToTwoBit in you PATH.
+
+# Usage (https://genome.ucsc.edu/goldenPath/help/blatSpec.html#faToTwoBitUsage)
+
+faToTwoBit GRCh38.dna.primary_assembly.fa GRCh38.dna.primary_assembly.2bit
+
